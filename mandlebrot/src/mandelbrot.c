5dd5d5906353b9d75f0d24aa1e78f327f234d5c0
Guy John <guy@rumblesan.com>
2012-08-24 13:07:45 +0100

    brot_create now puts data into brot struct

--- mandlebrot/src/mandelbrot.c
+++ mandlebrot/src/mandelbrot.c
@@ -2,14 +2,40 @@
 
 #include "mandelbrot.h"
 
-Mandelbrot brot_create(int pixWidth, int pixHeight, int repeats, float x1, float y1, float x2, float y2) {
+Mandelbrot brot_create(int pixelWidth, int pixelHeight, int repeats, float x1, float y1, float x2, float y2) {
 
     Mandelbrot brot = (Mandelbrot) malloc(sizeof(Mandelbrot_Data));
+
+    brot->x1 = x1;
+    brot->y1 = y1;
+
+    brot->x2 = x2;
+    brot->y2 = y2;
+
+    brot->pixelWidth = pixelWidth;
+    brot->pixelHeight = pixelHeight;
+
+    brot->pixels = (int**) malloc(sizeof(int*) * brot->pixelWidth);
+
+    // Assign the memory for the 2D array
+    // Actually done as an array of pointers to arrays of ints
+    int i;
+    for (i = 0; i < brot->pixelWidth; i++) {
+        brot->pixels[i] = (int*) malloc(sizeof(int*) * brot->pixelHeight);
+    }
 
     return brot;
 }
 
 void brot_cleanup(Mandelbrot brot) {
+
+    int i;
+
+    for (i = 0; i < brot->pixelWidth; i++) {
+        free(brot->pixels[i]);
+    }
+
+    free(brot->pixels);
 
     free(brot);
 }
